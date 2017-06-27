193a78094e5a4cc84f182fa4d740cc9bd237cf48
Isabella Teng <isabellateng@fb.com>
2017-06-27 15:28:51 -0700

    Added logout button

--- InstaPhoto/AccountViewController.swift
+++ InstaPhoto/AccountViewController.swift
@@ -6,30 +6,31 @@
 //  Copyright © 2017 Isabella Teng. All rights reserved.
 //
 
-import UIKit
+//TO DO: Add logout button
 
-class AccountViewController: UIViewController {
+import UIKit
+import Parse
+
+class AccountViewController: UIViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
 
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOutInBackground { (error: Error?) in
+            //PFUser.currentUser() will now be nil
+            print("User has logged out")
+            self.performSegue(withIdentifier: "logoutSegue", sender: nil )
+
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
