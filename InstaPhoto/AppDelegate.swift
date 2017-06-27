7c8e51376f2a1d1389e5df285f18b5c8b8c50603
Isabella Teng <isabellateng@fb.com>
2017-06-26 23:42:31 -0700

    Configured Parse server

--- InstaPhoto/AppDelegate.swift
+++ InstaPhoto/AppDelegate.swift
@@ -17,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Instaphoto"
+                configuration.clientKey = "Turtle98" // set to nil assuming you have not set clientKey
+                configuration.server = "https://shrouded-river-14118.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
