0b7353b59c8e4ae8588897aec13acc3c3bf7ad6a
Isabella Teng <isabellateng@fb.com>
2017-06-27 17:10:44 -0700

    Added button to allow user to choose from photo library

--- InstaPhoto/AddPhotoViewController.swift
+++ InstaPhoto/AddPhotoViewController.swift
@@ -39,23 +39,31 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
         
     }
 
+    @IBAction func uploadPhoto(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    
     //resize image for Parse
-    
-    
-//    func resize(image: UIImage, newSize: CGSize) -> UIImage {
-//        let resizeImageView = UIImageView(frame: CGRectMake(0, 0, newSize.width, newSize.height))
-//        resizeImageView.contentMode = UIViewContentMode.scaleAspectFill
-//        resizeImageView.image = image
-//        
-//        UIGraphicsBeginImageContext(resizeImageView.frame.size)
-//        resizeImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
-//        
-//        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-//        UIGraphicsEndImageContext()
-//        
-//        return newImage!
-//        
-//    }
+    func resize(image: UIImage, newSize: CGSize) -> UIImage {
+        let resizeImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        resizeImageView.contentMode = UIViewContentMode.scaleAspectFill
+        resizeImageView.image = image
+        
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+        
+    }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         //get the image captured by the UIImagePickerController
@@ -73,15 +81,4 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
